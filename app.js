55bb3e0526b3ff9a33aa3b006aac1c76c5fe8ab6
Jessica Birmingham <51299103+jlbirm@users.noreply.github.com>
2021-07-20 10:40:38 -0500

    refactor card creation

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 const mainTweets = document.getElementById("mainTweets");
 const enterTweets = document.getElementById("tweetForm");
 
-
-
-enterTweets.addEventListener('submit', function(e){
-    e.preventDefault();
-    let newTweet = document.querySelector('#tweetInput');
-    let newDiv = document.createElement('div');
-    newDiv.setAttribute("class", "card bg-dark");
-    let newCard = document.createElement('div');
-    newCard.setAttribute("class", "card-body");
-    let newQuote = document.createElement('blockquote');
-    newQuote.setAttribute("class", "blockquote mb-0");
+//Create card with blockquote and author information for each tweet
+let entry = function(newTweet){
     let p = document.createElement('p');
     p.innerText = newTweet.value;
     let footer = document.createElement('footer');
     footer.setAttribute("class", "blockquote-footer");
     //PLACEHOLDER FOR LOGIN NAME
-    footer.innerText = "AUTHOR" 
+    footer.innerText = "Author" 
+
+    let newQuote = document.createElement('blockquote');
+    newQuote.setAttribute("class", "blockquote mb-0");
     newQuote.appendChild(p);
     newQuote.appendChild(footer);
-    newCard.appendChild(newQuote);
-    newDiv.appendChild(newCard);
-    mainTweets.appendChild(newDiv);
 
+    let newCard = document.createElement('div');
+    newCard.setAttribute("class", "card-body");
+    newCard.appendChild(newQuote);
+
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute("class", "card bg-dark");
+    newDiv.appendChild(newCard);
+
+    return newDiv;
+}
+
+enterTweets.addEventListener('submit', function(e){
+    e.preventDefault();
+    let newTweet = document.querySelector('#tweetInput');
+    let newestEntry = entry(newTweet);
+
+    mainTweets.prepend(newestEntry);
+
+    //Reset the input box
     newTweet.value = '';
 })
-
